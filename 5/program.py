e306cce93bfc3d7e9c4a047bc3c41f7f60c514d2
Michael Andersson <michael.andersson@mojang.com>
2019-12-05 14:46:12 +0100

    5b

--- 5/program.py
+++ 5/program.py
@@ -11,15 +11,33 @@ PROGRAM_ADD_CODE=1
 PROGRAM_MULT_CODE=2
 PROGRAM_INPUT_CODE=3
 PROGRAM_OUTPUT_CODE=4
+
+PROGRAM_JUMP_TRUE_CODE=5
+PROGRAM_JUMP_FALSE_CODE=6
+PROGRAM_LESS_THAN_CODE=7
+PROGRAM_EQUALS_CODE=8
+
 PROGRAM_STOP_CODE=99
 
-KNOWN_OPERATOR_CODES = [PROGRAM_ADD_CODE, PROGRAM_MULT_CODE, PROGRAM_INPUT_CODE, PROGRAM_OUTPUT_CODE]
+KNOWN_OPERATOR_CODES = [PROGRAM_ADD_CODE, 
+                        PROGRAM_MULT_CODE, 
+                        PROGRAM_INPUT_CODE, 
+                        PROGRAM_OUTPUT_CODE,
+                        PROGRAM_JUMP_TRUE_CODE,
+                        PROGRAM_JUMP_FALSE_CODE,
+                        PROGRAM_LESS_THAN_CODE,
+                        PROGRAM_EQUALS_CODE,
+]
 
 OPERATOR_NUMBER_OF_PARAMETERS = {
     PROGRAM_ADD_CODE : 3,
     PROGRAM_MULT_CODE : 3,
     PROGRAM_INPUT_CODE : 1,
     PROGRAM_OUTPUT_CODE : 1,
+    PROGRAM_JUMP_TRUE_CODE : 2,
+    PROGRAM_JUMP_FALSE_CODE : 2,
+    PROGRAM_LESS_THAN_CODE : 3,
+    PROGRAM_EQUALS_CODE : 3,
     PROGRAM_STOP_CODE : 0
     }
 
@@ -71,6 +89,7 @@ def run_instruction(program, address_pointer, op_code, parameter_modes):
         value = val1 + val2
         destination = program[address_pointer+3]
         program[destination] = value
+        address_pointer = address_pointer + get_address_pointer_increment(op_code)
 
     elif op_code is PROGRAM_MULT_CODE:
         val1 = get_parameter_value(parameter_modes, 1, program, address_pointer)
@@ -78,20 +97,65 @@ def run_instruction(program, address_pointer, op_code, parameter_modes):
         value = val1 * val2
         destination = program[address_pointer+3]
         program[destination] = value
+        address_pointer = address_pointer + get_address_pointer_increment(op_code)
 
     elif op_code is PROGRAM_INPUT_CODE:
         destination = program[address_pointer+1]
         input_data = input("Enter your input value : ")
         program[destination] = input_data 
+        address_pointer = address_pointer + get_address_pointer_increment(op_code)
 
     elif op_code is PROGRAM_OUTPUT_CODE:
         destination = program[address_pointer+1]
         print("OPCODE_4: Value={}".format(program[destination]))
+        address_pointer = address_pointer + get_address_pointer_increment(op_code)
+
+    elif op_code is PROGRAM_JUMP_TRUE_CODE:
+        val1 = get_parameter_value(parameter_modes, 1, program, address_pointer)
+        val2 = get_parameter_value(parameter_modes, 2, program, address_pointer)
+        if val1 > 0: 
+            address_pointer = val2
+        else:
+            address_pointer = address_pointer + get_address_pointer_increment(op_code)
+
+    elif op_code is PROGRAM_JUMP_FALSE_CODE:
+        val1 = get_parameter_value(parameter_modes, 1, program, address_pointer)
+        val2 = get_parameter_value(parameter_modes, 2, program, address_pointer)
+        if val1 == 0: 
+            address_pointer = val2
+        else:
+            address_pointer = address_pointer + get_address_pointer_increment(op_code)
+
+    elif op_code is PROGRAM_LESS_THAN_CODE:
+        val1 = get_parameter_value(parameter_modes, 1, program, address_pointer)
+        val2 = get_parameter_value(parameter_modes, 2, program, address_pointer)
+
+        destination = program[address_pointer+3]
+            
+        if val1 < val2:
+            program[destination] = 1
+        else:
+            program[destination] = 0
+        
+        address_pointer = address_pointer + get_address_pointer_increment(op_code)
+
+    elif op_code is PROGRAM_EQUALS_CODE:
+        val1 = get_parameter_value(parameter_modes, 1, program, address_pointer)
+        val2 = get_parameter_value(parameter_modes, 2, program, address_pointer)
+
+        destination = program[address_pointer+3]
+            
+        if val1 == val2:
+            program[destination] = 1
+        else:
+            program[destination] = 0
+
+        address_pointer = address_pointer + get_address_pointer_increment(op_code)
 
     else:
         raise RuntimeError("op_code: {} is not a supported operation".format(op_code))
     
-    return program
+    return program, address_pointer
 
 def run_program(program):
     address_pointer = 0
@@ -99,8 +163,7 @@ def run_program(program):
     while(True):
         op_code, parameter_modes = parse_instruction(program, address_pointer)
         if op_code in KNOWN_OPERATOR_CODES:
-            program = run_instruction(program, address_pointer, op_code, parameter_modes) 
-            address_pointer = address_pointer + get_address_pointer_increment(op_code)
+            program, address_pointer = run_instruction(program, address_pointer, op_code, parameter_modes) 
         elif op_code is PROGRAM_STOP_CODE:
             break
         else:
@@ -108,11 +171,8 @@ def run_program(program):
             
     return program
 
-
-
 def a(input_data):
     output_program = run_program(input_data)
-    print("A, answer is: {}".format(output_program[OUTPUT_ADDRESS]))
 
 def b(input_data):
     pass
