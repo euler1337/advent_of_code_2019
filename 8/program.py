482ea09e464ca9192e220a0efec0b72f43a23309
Michael Andersson <michael.andersson@netlight.com>
2019-12-08 21:39:08 +0100

    8b

--- 8/program.py
+++ 8/program.py
@@ -5,6 +5,9 @@ import queue
 
 IMAGE_WIDTH = 25
 IMAGE_HEIGHT = 6
+#IMAGE_WIDTH = 3
+#IMAGE_HEIGHT = 2
+
 
 def a(input_data):
     number_of_layers =  int(len(input_data) / (IMAGE_WIDTH * IMAGE_HEIGHT))
@@ -25,16 +28,47 @@ def a(input_data):
 
     print("A, answer: {}, ones: {}, twos: {}".format(one_count[target_layer] * two_count[target_layer], one_count[target_layer], two_count[target_layer]))
 
-def b(input_data):
-    print(input_data)
+def determine_color(pixel_layers):
+    for x in pixel_layers:
+        if x == 1:
+            return 'X'
+        if x == 0:
+            return ' '
+        
 
+def b(input_data):
+    number_of_pixels = IMAGE_WIDTH * IMAGE_HEIGHT
+    number_of_layers =  int(len(input_data) / number_of_pixels)
+
+
+    all_pixel_data = []
+    for index in range(0, number_of_pixels):
+        lower_limit = index
+        upper_limit = len(input_data)
+        step_size = number_of_pixels
+        pixel_data = input_data[lower_limit : upper_limit : step_size]
+
+        print("pixel_data: {}".format(len(pixel_data)))
+        all_pixel_data.append(pixel_data)
+
+    pixel_colors = []
+    for pixel_data in all_pixel_data:
+        color = determine_color(pixel_data)
+        pixel_colors.append(color)
+
+    for row_index in range(0,IMAGE_HEIGHT):
+        lower = row_index * IMAGE_WIDTH
+        upper = (row_index + 1) * IMAGE_WIDTH
+        print(pixel_colors[lower:upper])
+
+        
 if __name__ == '__main__':
     f = open("input.txt", "r")
     input_data = f.readline()
     input_data = [int(x) for x in input_data]
     
     a(input_data.copy())
-    #b(input_data.copy())
+    b(input_data.copy())
     
 
 
