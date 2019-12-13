468edd911e71b61bf6c9853b4365a8accd7e09f6
Michael Andersson <michael.andersson@netlight.com>
2019-12-13 08:31:05 +0100

    B NOT YET ABLE TO FINISH THE GAME

--- 13/program.py
+++ 13/program.py
@@ -168,6 +168,7 @@ def get_relative_base(program_name = "default"):
 def run_instruction(program, address_pointer, op_code, parameter_modes, mode):
 
     output_written = False
+    input_received = False
 
     #print("RUNNING INSTRUCTION: op_code={}".format(op_code))
 
@@ -194,12 +195,19 @@ def run_instruction(program, address_pointer, op_code, parameter_modes, mode):
         if mode == NON_INTERACTIVE_MODE:  
             input_data = get_program_input()
         elif mode == INTERACTIVE_MODE: 
-            input_data = input("Enter your input value : ")
+            input_data = ''
+            while len(input_data) == 0:
+                input_data = input("Enter your input value : ")
+                if input_data == "a":
+                    input_data = "-1"
+                elif input_data == "d":
+                    input_data = "1"
         else:
             raise(RuntimeError("DID NOT RECOGNIZE MODE: {}".format(mode)))
             
         write_data(parameter_modes[0], program, destination, input_data) 
         address_pointer = address_pointer + get_address_pointer_increment(op_code)
+        input_received = True
 
     elif op_code is PROGRAM_OUTPUT_CODE:
         output_value = get_parameter_value(parameter_modes, 1, program, address_pointer)
@@ -258,15 +266,15 @@ def run_instruction(program, address_pointer, op_code, parameter_modes, mode):
     else:
         raise RuntimeError("op_code: {} is not a supported operation".format(op_code))
     
-    return program, address_pointer, output_written
+    return program, address_pointer, output_written, input_received
 
 def run_program(program, mode, address_pointer=0):
     done = False
     while(True):
         op_code, parameter_modes = parse_instruction(program, address_pointer)
         if op_code in KNOWN_OPERATOR_CODES:
-            program, address_pointer, output_written = run_instruction(program, address_pointer, op_code, parameter_modes, mode) 
-            if output_written:
+            program, address_pointer, output_written, input_received = run_instruction(program, address_pointer, op_code, parameter_modes, mode) 
+            if input_received:
                 break
         elif op_code is PROGRAM_STOP_CODE:
             done = True
@@ -278,6 +286,7 @@ def run_program(program, mode, address_pointer=0):
 
 Coordinate = namedtuple('Coordinate', 'x, y')
 OUTPUT_MAP = {}
+PROGRAM_DATA = {}
 
 def parse_program_output():
     output = []
@@ -290,7 +299,11 @@ def parse_program_output():
 def draw(tile, x, y):
     #print("Tile: {}, X: {}, Y:{}".format(tile, x, y))
     c = Coordinate(x,y)
-    if str(tile) in "01234":
+
+    if x == -1 and y == 0:
+        PROGRAM_DATA["score"] = tile
+        print("SCORE: {}".format(tile))
+    elif str(tile) in "01234":
         OUTPUT_MAP[c] = tile
     else:
         raise(RuntimeError("INVALID TILE: {}".format(tile)))
@@ -304,10 +317,16 @@ def parse_game(game_program):
 
     address_pointer = 0
     done = False
+    x_max = 0
+    y_max = 0
+    print("Parsing program with length: {}".format(len(game_program)))
     while not done:
         x = game_program[address_pointer]
         y = game_program[address_pointer+1]
         tile = game_program[address_pointer+2]
+
+        x_max = max(x_max,x)
+        y_max = max(y_max,y)
 
         draw(tile, x, y)
         address_pointer = address_pointer + 3
@@ -315,6 +334,9 @@ def parse_game(game_program):
         if address_pointer >= len(game_program) - 1:
             #print("Done parsing program: address_ptr: {}, program_len = {}".format(address_pointer, len(game_program)))
             done = True
+
+    PROGRAM_DATA["x_max"] = max(PROGRAM_DATA["x_max"], x_max)
+    PROGRAM_DATA["y_max"] = max(PROGRAM_DATA["y_max"], y_max)
 
 
 def a(input_data):
@@ -332,9 +354,50 @@ def a(input_data):
     print("A, answer: {}".format(tile_count))
         
 
-def b(input_data):
+def print_tile(tile):
+    if tile == 0:
+        return " "
+    elif tile == 1:
+        return "-"
+    elif tile == 2:
+        return "o"
+    elif tile == 3:
+        return "-"
+    elif tile == 4:
+        return "X"
+
+def show_map():
+    x_max = PROGRAM_DATA["x_max"]
+    y_max = PROGRAM_DATA["y_max"]
+    print("X_MAX: {}, Y_MAX: {}".format(x_max, y_max))
+    matrix = [[" " for x in range(x_max+1)] for y in range(y_max+1)]
+    # [y][x]
+
+    for c, tile in OUTPUT_MAP.items():
+        matrix[c.y][c.x] = print_tile(tile)
     
-    pass
+    #for y in range(y_max,-1,-1):
+    for y in range(y_max):
+        print("".join(matrix[y]))
+
+def b(input_data):
+    address_pointer = 0
+    program = input_data
+    done = False
+
+    program[0] = 2
+    PROGRAM_DATA["x_max"] = 0
+    PROGRAM_DATA["y_max"] = 0
+
+
+    while not done:
+        program, done, address_pointer = run_program(program, INTERACTIVE_MODE, address_pointer)
+        game_program = parse_program_output()
+        parse_game(game_program)
+        show_map()
+        #OUTPUT_MAP.clear()
+
+    print("GAME OVER: SCORE: {}".format(PROGRAM_DATA["score"]))
 
 if __name__ == '__main__':
     f = open("input.txt", "r")
@@ -342,7 +405,7 @@ if __name__ == '__main__':
     input_data = [int(x) for x in input_data]
     PROGRAM_RELATIVE_BASE["default"] = 0
 
-    a(input_data.copy())
+    #a(input_data.copy())
     b(input_data.copy())
     
 
